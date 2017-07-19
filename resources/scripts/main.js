63f2e3fc6cafc4d59c46e91ef8c77ce8c155043d
Katie <katherinelane11@gmail.com>
2017-07-19 15:43:05 -0400

    changes first powerball to random number

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -15,22 +15,25 @@
 // 5 slots
 // user must press button to innitiate thing (event listener)
 // generage a random number between 1-69  (math.round)
-// choose one at a time and enter them into the html shown to user (probably using textContent property)
+// choose one at a time and enter them into the html shown to user (probably using textContent property) 
+// want to replace h1 with random number generated
 // as loops through and fills slots on html: 
     // if number is same as previous number(s) choose another
+
 // stop when all 5 slots are filled --> then do next two slots of numbers
 
 function getButton(){
-    var button = document.getElementsByClassName("click-me");
-    var buttonArray = [].slice.call(button)
-    return buttonArray;
+    var button = document.querySelector("button");
+    // var buttonArray = [].slice.call(button)
+    return button;
 };
 
 function clickButton(min, max){
     var clickMeBtn = getButton();
     clickMeBtn.addEventListener("click", function (event) {
         event.preventDefault();
-        return getRandomFirstNumbers(min, max);
+        var randomNumber = getRandomFirstNumbers(min, max);
+        getNumberSlot(randomNumber);
     });
 };
 
@@ -41,3 +44,15 @@ function getRandomFirstNumbers(min, max) {
 };
 
 
+
+function getNumberSlot(randomNumber){
+    // have all number slots (h1)
+    // have a random number generated
+    var numberSlots = document.querySelectorAll("[data-text-role='number']");
+    // print number to HTML
+    numberSlots.textContent.forEach(
+        numberSlots[0].textContent = randomNumber);
+    // return number;
+}
+
+clickButton(1, 69);
